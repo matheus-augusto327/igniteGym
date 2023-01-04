243828472df6be5018d055d4fb841a5b2222ab34
Matheus <matheus.augustoreal@gmail.com>
2023-01-04 08:36:53 -0300

    feat: customizing the default theme

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
+import { NativeBaseProvider } from "native-base";
 import { Text, View, StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { NativeBaseProvider } from "native-base";
+
+import { THEME } from "src/theme";
 import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
