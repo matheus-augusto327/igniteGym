7f831734203698513b72f07ac2cfa73b3ce83974
Matheus <matheus.augustoreal@gmail.com>
2023-01-05 19:41:39 -0300

    feat: create Button component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,6 +3,7 @@ import { VStack, Image, Text, Center, Heading } from "native-base";
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
+import { Button } from "@components/button";
 
 export function SignIn() {
   return (
@@ -31,6 +32,8 @@ export function SignIn() {
           autoCapitalize="none"
         />
         <Input placeholder="Senha" secureTextEntry />
+
+        <Button title="Acessar" />
       </Center>
     </VStack>
   );
