ac86a2e80d2158c29ab8bcd57d87aec6baad1e70
Matheus <matheus.augustoreal@gmail.com>
2023-01-05 20:09:05 -0300

    feat: create navigation context

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import {
 
 import { THEME } from "src/theme";
 import { Loading } from "@components/Loading";
-import { SignIn } from "@screens/Signin";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
