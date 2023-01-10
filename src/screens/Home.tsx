01d85b16129217be48a220279d2d403adc7eb6eb
Matheus <matheus.augustoreal@gmail.com>
2023-01-10 08:55:27 -0300

    style: Pressable press event

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,15 @@
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack, Text } from "native-base";
+import { VStack, Text, HStack } from "native-base";
 
 export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <Group name="Costas" />
+      <HStack>
+        <Group name="Costas" />
+        <Group name="Ombro" />
+      </HStack>
     </VStack>
   );
 }
