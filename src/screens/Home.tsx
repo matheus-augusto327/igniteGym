e1ed7fa5b52b7ee9f85c404fdae550f38d5a6e74
Matheus <matheus.augustoreal@gmail.com>
2023-01-12 10:04:40 -0300

    feat: exercises screen navigation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,8 @@
 import { ExerciseCard } from "@components/ExerciseCard";
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
 import { VStack, FlatList, HStack, Heading, Text } from "native-base";
 import { useState } from "react";
 
@@ -18,6 +20,12 @@ export function Home() {
     "Roca alternada",
   ]);
   const [groupSelected, setGroupSelected] = useState("Costas");
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate("exercise");
+  }
 
   return (
     <VStack flex={1}>
@@ -56,7 +64,9 @@ export function Home() {
         <FlatList
           data={exercises}
           keyExtractor={(item) => item}
-          renderItem={({ item }) => <ExerciseCard />}
+          renderItem={({ item }) => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{ paddingBottom: 20 }}
         />
