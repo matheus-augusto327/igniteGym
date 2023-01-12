8783a68466bae871e9f04e7f93beeb82dc5b57fc
Matheus <matheus.augustoreal@gmail.com>
2023-01-12 08:20:12 -0300

    feat: create history section list

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,35 @@
+import { Hisotrycard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack } from "native-base";
+import { Heading, VStack, SectionList } from "native-base";
+import { useState } from "react";
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "26.02.22",
+      data: ["Puxada frontal", "Remada unilateral"],
+    },
+    {
+      title: "27.02.22",
+      data: ["Puxada frontal"],
+    },
+  ]);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <Hisotrycard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
     </VStack>
   );
 }
