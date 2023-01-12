789c25ce4d39970bed4616011d9c5de22951d23c
Matheus <matheus.augustoreal@gmail.com>
2023-01-12 08:36:47 -0300

    feat: user photo skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,18 +1,33 @@
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, ScrollView, VStack } from "native-base";
+import { Center, ScrollView, Skeleton, VStack } from "native-base";
+import { useState } from "react";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(true);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: "https://matheus-augusto327.png" }}
-            alt="Foto do usuário"
-            size={33}
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://matheus-augusto327.png" }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
         </Center>
       </ScrollView>
     </VStack>
