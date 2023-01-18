5f2021763158115d21a6f2c2027462352f6f0f41
Matheus <matheus.augustoreal@gmail.com>
2023-01-18 08:24:43 -0300

    feat: create custom hook

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,14 +1,12 @@
 import { Box, useTheme } from "native-base";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
-import { AppRoutes } from "./app.routes";
-import { useContext } from "react";
-import { AuthContext } from "src/context/AuthContext";
+import { useAuth } from "@hooks/useAuth";
 
 export function Routes() {
-  const contextData = useContext(AuthContext);
-
   const { colors } = useTheme();
+  const { user } = useAuth();
+
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
