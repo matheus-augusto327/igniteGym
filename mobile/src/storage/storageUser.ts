b40091d16c23def72828313087c188b5eebc4f8b
Matheus <matheus.augustoreal@gmail.com>
2023-01-19 16:45:20 -0300

    feat: persisting user token

--- mobile/src/storage/storageUser.ts
+++ mobile/src/storage/storageUser.ts
@@ -1,6 +1,6 @@
 import { UserDTO } from "@dtos/UserDTO";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { USER_STORAGE } from "./storage.config";
+import { USER_STORAGE } from "./storageConfig";
 
 export async function storageUserSave(user: UserDTO) {
   await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(user));
