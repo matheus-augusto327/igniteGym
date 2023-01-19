a920a4ad38977e3b9f4b4eecd6ddcbb7c3fec45a
Matheus <matheus.augustoreal@gmail.com>
2023-01-19 07:31:09 -0300

    feat: updating context states

--- mobile/src/context/AuthContext.tsx
+++ mobile/src/context/AuthContext.tsx
@@ -1,8 +1,9 @@
 import { UserDTO } from "@dtos/UserDTO";
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 export type AuthContextDataProps = {
   user: UserDTO;
+  signIn: (email: string, password: string) => void;
 };
 
 type AuthContextProviderProps = {
@@ -14,17 +15,19 @@ export const AuthContext = createContext<AuthContextDataProps>(
 );
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState({
+    id: "1",
+    name: "Lucão",
+    email: "lucao@gmail.com",
+    avatar: "lucao.png",
+  });
+
+  function signIn(email: string, password: string) {
+    setUser({ id: "", name: "", email, avatar: "" });
+  }
+
   return (
-    <AuthContext.Provider
-      value={{
-        user: {
-          id: "1",
-          name: "Lucão",
-          email: "lucao@gmail.com",
-          avatar: "lucao.png",
-        },
-      }}
-    >
+    <AuthContext.Provider value={{ user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -5,12 +5,31 @@ import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/button";
 import { useNavigation } from "@react-navigation/native";
+import { Controller, useForm } from "react-hook-form";
+import { useAuth } from "@hooks/useAuth";
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 export function SignIn() {
+  const { signIn } = useAuth();
+
   const navigation = useNavigation<AuthNavigatorRouteProps>();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   function handleNewAccount() {
     navigation.navigate("signUp");
+  }
+
+  function handleSignIn({ email, password }: FormData) {
+    signIn(email, password);
   }
 
   return (
@@ -18,7 +37,7 @@ export function SignIn() {
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} px={10}>
+      <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackgroundImg}
           defaultSource={BackgroundImg}
@@ -26,39 +45,56 @@ export function SignIn() {
           resizeMode="contain"
           position="absolute"
         />
-
         <Center my={24}>
           <LogoSvg />
           <Text color="gray.100" fontSize="sm">
-            Treine sua mente e seu corpo
+            Treine sua mente e o seu corpo.
           </Text>
         </Center>
-
         <Center>
           <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-            Acesse sua conta
+            Acesse a conta
           </Heading>
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+          <Controller
+            control={control}
+            name="email"
+            rules={{ required: "Informe o e-mail" }}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                errorMessage={errors.email?.message}
+              />
+            )}
           />
-          <Input placeholder="Senha" secureTextEntry />
 
-          <Button title="Acessar" />
+          <Controller
+            control={control}
+            name="password"
+            rules={{ required: "Informe a senha" }}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+          <Button title="Acessar" onPress={handleSubmit(handleSignIn)} />
         </Center>
-
         <Center mt={24}>
           <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
             Ainda não tem acesso?
           </Text>
+          <Button
+            title="Criar Conta"
+            variant="outline"
+            onPress={handleNewAccount}
+          />
         </Center>
-
-        <Button
-          title="Criar conta"
-          variant="outline"
-          onPress={handleNewAccount}
-        />
       </VStack>
     </ScrollView>
   );
