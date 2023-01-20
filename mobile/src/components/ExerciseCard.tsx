d7b4c0ddf14df8255b142889c7bbd9f860c4a12c
Matheus <matheus.augustoreal@gmail.com>
2023-01-20 09:22:29 -0300

    feat: displayng exercises by group

--- mobile/src/components/ExerciseCard.tsx
+++ mobile/src/components/ExerciseCard.tsx
@@ -1,10 +1,13 @@
 import { Heading, HStack, Image, VStack, Text, Icon } from "native-base";
 import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { Entypo } from "@expo/vector-icons";
+import { ExerciseDTO } from "@dtos/ExerciseDTO";
 
-type Props = TouchableOpacityProps & {};
+type Props = TouchableOpacityProps & {
+  data: ExerciseDTO;
+};
 
-export function ExerciseCard({ ...rest }: TouchableOpacityProps) {
+export function ExerciseCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity {...rest}>
       <HStack
@@ -28,10 +31,10 @@ export function ExerciseCard({ ...rest }: TouchableOpacityProps) {
         />
         <VStack flex={1}>
           <Heading fontSize="lg" color="white" fontFamily="heading">
-            Remada unilateral
+            {data.name}
           </Heading>
           <Text fontSize="sm" color="gray.200" mt={1} numberOfLines={2}>
-            3 séries x 1 repetições
+            {data.series} séries x {data.repetitions} repetições
           </Text>
         </VStack>
         <Icon as={Entypo} name="chevron-thin-right" color="gray.300" />
