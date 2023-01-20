18afe0d10a4d06242b9a410134f9e6a76c9d4c68
Matheus <matheus.augustoreal@gmail.com>
2023-01-20 10:25:17 -0300

    feat: exercise screen loading

--- mobile/src/screens/Exercise.tsx
+++ mobile/src/screens/Exercise.tsx
@@ -22,12 +22,14 @@ import { AppError } from "@utils/AppError";
 import { api } from "src/service/api";
 import { useEffect, useState } from "react";
 import { ExerciseDTO } from "@dtos/ExerciseDTO";
+import { Loading } from "@components/Loading";
 
 type RouteParamsProps = {
   exerciseId: string;
 };
 
 export function Exercise() {
+  const [isLoading, setIsLoading] = useState(true);
   const [exercise, setExercise] = useState<ExerciseDTO>({} as ExerciseDTO);
   const navigation = useNavigation<AppNavigatorRoutesProps>();
 
@@ -42,6 +44,7 @@ export function Exercise() {
 
   async function fetchExerciseDetails() {
     try {
+      setIsLoading(true);
       const response = await api.get(`/exercises/${exerciseId}`);
       setExercise(response.data);
     } catch (error) {
@@ -51,6 +54,8 @@ export function Exercise() {
         : "Não foi possível carregar os detalhes do exercício.";
 
       toast.show({ title, placement: "top", bgColor: "red.500" });
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -88,46 +93,51 @@ export function Exercise() {
           </HStack>
         </HStack>
       </VStack>
-      <ScrollView>
-        <VStack p={8}>
-          <Box rounded="lg" mb={3} overflow="hidden">
-            <Image
-              w="full"
-              h={80}
-              source={{
-                uri: `${api.defaults.baseURL}/exercises/demo/${exercise.demo}`,
-              }}
-              alt="Nome do exercício"
-              resizeMode="cover"
-              rounded="lg"
-            />
-          </Box>
-          <Box bg="gray.600" rounded="md" pb={4} px={4}>
-            <HStack
-              alignItems="center"
-              justifyContent="space-around"
-              mb={6}
-              mt={5}
-            >
-              <HStack>
-                <SeriesSvg />
-                <Text color="gray.200" ml={2}>
-                  {exercise.series} séries
-                </Text>
+
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <ScrollView>
+          <VStack p={8}>
+            <Box rounded="lg" mb={3} overflow="hidden">
+              <Image
+                w="full"
+                h={80}
+                source={{
+                  uri: `${api.defaults.baseURL}/exercises/demo/${exercise.demo}`,
+                }}
+                alt="Nome do exercício"
+                resizeMode="cover"
+                rounded="lg"
+              />
+            </Box>
+            <Box bg="gray.600" rounded="md" pb={4} px={4}>
+              <HStack
+                alignItems="center"
+                justifyContent="space-around"
+                mb={6}
+                mt={5}
+              >
+                <HStack>
+                  <SeriesSvg />
+                  <Text color="gray.200" ml={2}>
+                    {exercise.series} séries
+                  </Text>
+                </HStack>
+
+                <HStack>
+                  <RepetitionsSvg />
+                  <Text color="gray.200" ml={2}>
+                    {exercise.repetitions} repetições
+                  </Text>
+                </HStack>
               </HStack>
 
-              <HStack>
-                <SeriesSvg />
-                <Text color="gray.200" ml={2}>
-                  {exercise.repetitions} repetições
-                </Text>
-              </HStack>
-            </HStack>
-
-            <Button title="Marcar como realizado" />
-          </Box>
-        </VStack>
-      </ScrollView>
+              <Button title="Marcar como realizado" />
+            </Box>
+          </VStack>
+        </ScrollView>
+      )}
     </VStack>
   );
 }
