7d0723b86c4d9a162c1d5cd44143f6012cad8b95
Matheus <matheus.augustoreal@gmail.com>
2023-01-23 08:06:27 -0300

    feat: including Token manager in API

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,19 +1,31 @@
 import { AppError } from "@utils/AppError";
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+
+type SignOut = () => void;
+
+type APIInstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void;
+};
 
 const api = axios.create({
   baseURL: "http://192.168.15.1:3333",
-});
+}) as APIInstanceProps;
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.data) {
-      return Promise.reject(new AppError(error.response.data.message));
-    } else {
-      return Promise.reject(new AppError(error));
+api.registerInterceptTokenManager = (signOut) => {
+  const interceptTokenManager = api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response && error.response.data) {
+        return Promise.reject(new AppError(error.response.data.message));
+      } else {
+        return Promise.reject(new AppError(error));
+      }
     }
-  }
-);
+  );
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager);
+  };
+};
 
 export { api };
